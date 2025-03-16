8066c2454d2b1ecf4984052222a92199cdd52de3
YannCrt <yanncretin75@gmail.com>
2025-03-16 22:45:39 +0100

    Update css login and signup pages

--- animemaniashop/src/app/login/page.jsx
+++ animemaniashop/src/app/login/page.jsx
@@ -2,40 +2,24 @@ import React from "react";
 
 function page() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-lg w-96">
-        <h2 className="text-2xl font-bold text-gray-700 mb-6 text-center">
-          Connexion
-        </h2>
+    <div className="login-page ">
+      <div className="login-container">
+        <h2>Connexion</h2>
         <form>
-          <div className="mb-4">
-            <label className="block text-gray-700">Email</label>
-            <input
-              type="email"
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
-              placeholder="Entrez votre email"
-            />
+          <div className="login-input">
+            <label>Email</label>
+            <input type="email" placeholder="Entrez votre email" />
           </div>
-          <div className="mb-4">
-            <label className="block text-gray-700">Mot de passe</label>
-            <input
-              type="password"
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
-              placeholder="Entrez votre mot de passe"
-            />
+          <div className="login-input">
+            <label>Mot de passe</label>
+            <input type="password" placeholder="Entrez votre mot de passe" />
           </div>
-          <button
-            type="submit"
-            className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition duration-200"
-          >
+          <button type="submit" className="login-button">
             Se connecter
           </button>
         </form>
-        <p className="text-center text-sm text-gray-600 mt-4">
-          Pas encore inscrit ?{" "}
-          <a href="/signup" className="text-blue-500 hover:underline">
-            Créer un compte
-          </a>
+        <p className="login-footer">
+          Pas encore inscrit ? <a href="/signup">Créer un compte</a>
         </p>
       </div>
     </div>
--- animemaniashop/src/app/signup/page.jsx
+++ animemaniashop/src/app/signup/page.jsx
@@ -2,51 +2,34 @@ import React from "react";
 
 function page() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-white p-6 rounded-lg shadow-md w-96">
-        <h2 className="text-2xl font-semibold text-center mb-4">Inscription</h2>
+    <div className="login-page">
+      <div className="login-container">
+        <h2>Inscription</h2>
         <form>
-          <div className="mb-4">
+          <div className="login-input">
             <label className="block text-gray-700">Prénom</label>
-            <input
-              type="text"
-              className="w-full px-3 py-2 border rounded-lg"
-              placeholder="Votre prénom"
-            />
+            <input type="text" placeholder="Votre prénom" />
           </div>
-          <div className="mb-4">
-            <label className="block text-gray-700">Nom</label>
+          <div className="login-input">
+            <label>Nom</label>
             <input
               type="text"
               className="w-full px-3 py-2 border rounded-lg"
               placeholder="Votre nom"
             />
           </div>
-          <div className="mb-4">
-            <label className="block text-gray-700">Email</label>
-            <input
-              type="email"
-              className="w-full px-3 py-2 border rounded-lg"
-              placeholder="Votre email"
-            />
+          <div className="login-input">
+            <label>Email</label>
+            <input type="email" placeholder="Votre email" />
           </div>
-          <div className="mb-4">
-            <label className="block text-gray-700">Mot de passe</label>
-            <input
-              type="password"
-              className="w-full px-3 py-2 border rounded-lg"
-              placeholder="Votre mot de passe"
-            />
+          <div className="login-input">
+            <label>Mot de passe</label>
+            <input type="password" placeholder="Votre mot de passe" />
           </div>
-          <button className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600">
-            S'inscrire
-          </button>
+          <button className="login-button">S'inscrire</button>
         </form>
-        <p className="text-center text-gray-600 mt-4">
-          Déjà un compte ?{" "}
-          <a href="/login" className="text-blue-500">
-            Se connecter
-          </a>
+        <p className="login-footer">
+          Déjà un compte ? <a href="/login">Se connecter</a>
         </p>
       </div>
     </div>
