bd2e62129af1bfe238dcefb82598ea8f41b6e11c
Santiago NARANJO GRAJALES <snaranjograjales@turgot-paris.fr>
2025-03-18 11:59:11 +0100

    ajout de liens sur les images

--- animemaniashop/lib/product.action.js
+++ animemaniashop/lib/product.action.js
@@ -5,3 +5,11 @@ import { prisma } from "./prisma";
 export async function getAllProducts() {
   return await prisma.product.findMany();
 }
+
+export const getProductById = async (id) => {
+  const product = await prisma.product.findUnique({
+    where: { id: Number(id) }, // L'ID doit être un nombre
+  });
+
+  return product;
+};
--- animemaniashop/src/app/shop/page.jsx
+++ animemaniashop/src/app/shop/page.jsx
@@ -1,5 +1,6 @@
 "use server";
 import Image from "next/image";
+import Link from "next/link";
 import { getAllProducts } from "../../../lib/product.action";
 
 export default async function ShopPage() {
@@ -10,12 +11,14 @@ export default async function ShopPage() {
       <div className="products">
         {products.map((product) => (
           <div key={product.id}>
-            <Image
-              src={product.image_url}
-              alt={product.name}
-              height={200}
-              width={200}
-            />
+            <Link href={`/product/${product.id}`}>
+              <Image
+                src={product.image_url}
+                alt={product.name}
+                height={200}
+                width={200}
+              />
+            </Link>
             <div>{product.name} </div>
             <div>{product.description} </div>
             <div>Prix : {product.price} €</div>
